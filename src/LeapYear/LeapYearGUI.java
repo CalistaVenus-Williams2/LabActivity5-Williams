ca735e12ba15e45abb6a13e00772dfe6a98b4115
w-cali <calista.flores.williams@gmail.com>
2022-12-16 20:10:36 +0800

    rename LeapYear components (forgot to change gui)

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -2,9 +2,12 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
     //idk if apil ang option pane sa rename???? so....
+    //ah thats embarrassing i forgot to actually rename the gui components LMAO
+
 }
+
