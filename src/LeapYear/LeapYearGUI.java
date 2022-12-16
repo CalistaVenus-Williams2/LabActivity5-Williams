77abf86fdea769c18942a4264411f7ec36e9846e
w-cali <calista.flores.williams@gmail.com>
2022-12-16 21:19:39 +0800

    complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,6 +1,8 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI extends JFrame{
     private JPanel panel1;
@@ -8,15 +10,41 @@ public class LeapYearGUI extends JFrame{
     private JButton btnCheckYear;
 
     public LeapYearGUI() {
-        //things that this does
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                display(determineLeapYear());
+            }
+        });
     }
 
     public static void main(String[] args){
         LeapYearGUI app = new LeapYearGUI();
         app.setContentPane(app.panel1);
-        app.setSize(500,500);
+        app.setSize(250,250);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
     }
+
+    public boolean determineLeapYear(){
+        int year = Integer.parseInt(tfYear.getText());
+        if(year%4==0){
+            if(year%100==0){
+                if(year%400==0){
+                    return true;
+                } return false;
+            } return true;
+        } return false;
+    }
+
+    public void display(boolean leap){
+        JFrame huh = new JFrame();
+        if(leap){
+            JOptionPane.showMessageDialog(huh,"Leap Year");
+        } else {
+            JOptionPane.showMessageDialog(huh,"Not a leap year");
+        }
+    }
 }
+
 
