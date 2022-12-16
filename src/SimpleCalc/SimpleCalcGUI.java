8d7f118f5a85566f6282f8e90a53b5c3b50c888a
w-cali <calista.flores.williams@gmail.com>
2022-12-17 00:53:36 +0800

    add SimpleCalc main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,12 +1,24 @@
 package SimpleCalc;
 
+
 import javax.swing.*;
 
-public class SimpleCalcGUI {
+public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfNumber1;
     private JTextField tfNumber2;
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JLabel lblResult;
+
+    SimpleCalcGUI(){
+        //perform actions here
+    }
+    public static void main(String[] args){
+        SimpleCalcGUI app = new SimpleCalcGUI();
+        app.setContentPane(app.panel1);
+        app.setSize(600,250);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
