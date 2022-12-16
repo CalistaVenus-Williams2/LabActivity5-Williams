93cc8f7fdebe467ac84d4974d577fa99ad210ee0
w-cali <calista.flores.williams@gmail.com>
2022-12-16 20:15:31 +0800

    add LeapYear main function

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -6,8 +6,17 @@ public class LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
-    //idk if apil ang option pane sa rename???? so....
-    //ah thats embarrassing i forgot to actually rename the gui components LMAO
 
+    public LeapYearGUI() {
+        //things that this does
+    }
+
+    public static void main(String[] args){
+        LeapYearGUI app = new LeapYearGUI();
+        app.setContentPane(app.panel1);
+        app.setSize(500,500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
 
